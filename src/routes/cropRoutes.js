5b2bad78946ed36f216ad6e7341ff3d60ec98c48
Sayanduary <sayanduary@gmail.com>
2025-04-12 22:48:17 +0530

    done

---
+++ src/routes/cropRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { getCropRecommendation } from '../controllers/cropController';
+
+const router = express.Router();
+
+router.post('/recommend', getCropRecommendation);
+
+export default router;
