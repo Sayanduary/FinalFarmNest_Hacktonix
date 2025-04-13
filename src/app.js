6113c19837782d698edadcb1c8106f7ddbf7c1d8
Sayanduary <sayanduary@gmail.com>
2025-04-13 09:40:16 +0530

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import authRouter from './routes/authRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js'
 import cropRoute from './routes/cropRoutes.js'
-
+import path from "path";
+import { fileURLToPath } from "url";
 
 
 const app = express();
@@ -21,6 +22,12 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+
 
 app.use('/api/v1/auth', authRouter);
 app.use("/api/v1/category", categoryRoutes);
